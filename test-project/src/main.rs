570cb3fd50c78ba5dbc470ff23dbee97dc3d991a
Matheus Amendola <matheusamendolaa@gmail.com>
2023-05-26 23:57:11 +0000

    tentando

--- test-project/src/main.rs
+++ test-project/src/main.rs
@@ -1,9 +1,42 @@
-/*--------------------------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
- *-------------------------------------------------------------------------------------------------------------*/
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use std::error::Error;
 
-fn main() {
-    let name = "VS Code Remote - Containers";
-    println!("Hello, {}!", name);
+#[derive(Debug, Serialize, Deserialize)]
+struct Horoscope {
+    sign: String,
+    message: String,
+    lucky_number: Option<u32>,
+    lucky_animal: Option<String>,
+}
+
+async fn get_horoscope(sign: &str, plan: &str) -> Result<Horoscope, Box<dyn Error>> {
+    let client: Client = Client::new();
+    let url: &str = "https://matheusamendola-super-engine-q96gv49g7xvcg77-5000.preview.app.github.dev/horoscope";
+
+    let body: serde_json::Value = json!({
+        "sign": sign,
+        "plan": plan,
+    });
+
+    let response: reqwest::Response = client.post(url).json(&body).send().await?;
+    let horoscope: Horoscope = response.json().await?;
+
+    Ok(horoscope)
+}
+
+#[tokio::main]
+async fn main() {
+    let sign: &str = "Áries";
+    let plan: &str = "advanced";
+
+    match get_horoscope(sign, plan).await {
+        Ok(horoscope) => {
+            println!("Horoscope: {:?}", horoscope);
+        }
+        Err(err) => {
+            eprintln!("Error: {}", err);
+        }
+    }
 }
